573f4c3f532ad03e496510105686a942b3b39e30
kingsley <ndynoahs@gmail.com>
2024-01-17 17:57:38 +0100

    first design round up

--- src/Pages.js/Homepage.js
+++ src/Pages.js/Homepage.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import  "../assets/styles.css"
 import ProductList from "../components/productList/ProductList"
-import heroImage from "../assets/images/kingsley-image.jpg"
+// import heroImage from "../assets/images/kingsley-image.jpg"
+import heroImage from "../assets/images/profile_img2.jpeg"
+
 import image from "../assets/images/waving-hand.png"
 import image1 from "../assets/images/html-css-icons.svg"
 import image2 from "../assets/images/react nextjs-icons.svg"
@@ -21,39 +23,12 @@ const Homepage = () => {
   return (
     <div className=''>
       <Navbar />
-
-      {/* <nav className='nav'>
-        <div className='nav-items'> 
-        <h2>
-          Kings.dev
-        </h2>
-        <ul className='nav-list'>
-          <li>
-            <a href='#home'>Home </a>
-          </li>
-          <li >
-            <a href="#about">About </a>
-          </li>
-          <li>
-            <a href="#projects">Projects </a>
-          </li>
-          <li>
-            
-            <a href="#contact">Contact </a>
-
-          </li>
-
-        </ul>
-        </div>
-      </nav> */}
-
       <section id="home" className='home'>
-        <div className='container'> 
           <div className='content'>
             <div className='hero-main'>
               <div className='hero-text'>
-                <h1 className='header-lg' > Front-End React <br/> Developer <img src={image} alt="waving-hand" /></h1>
-                <h6>Hi, I'm Kingsley Ibe. A passionate Front-end React <br/> Developer based in Lagos, Nigeria. 📍</h6>
+                <h1 className='header_lg' > Front-End React <br/> Developer <img className='hand_icon' src={image} alt="waving-hand" /></h1>
+                <p className='header_sm'>Hi, I'm Kingsley Ibe. A passionate Front-end React <br/> Developer based in Lagos, Nigeria. 📍</p>
                 <span className='hero-icons'>
                   <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="60" height="60" viewBox="0 0 50 50">
                     <path d="M 9 4 C 6.2504839 4 4 6.2504839 4 9 L 4 41 C 4 43.749516 6.2504839 46 9 46 L 41 46 C 43.749516 46 46 43.749516 46 41 L 46 9 C 46 6.2504839 43.749516 4 41 4 L 9 4 z M 9 6 L 41 6 C 42.668484 6 44 7.3315161 44 9 L 44 41 C 44 42.668484 42.668484 44 41 44 L 9 44 C 7.3315161 44 6 42.668484 6 41 L 6 9 C 6 7.3315161 7.3315161 6 9 6 z M 14 11.011719 C 12.904779 11.011719 11.919219 11.339079 11.189453 11.953125 C 10.459687 12.567171 10.011719 13.484511 10.011719 14.466797 C 10.011719 16.333977 11.631285 17.789609 13.691406 17.933594 A 0.98809878 0.98809878 0 0 0 13.695312 17.935547 A 0.98809878 0.98809878 0 0 0 14 17.988281 C 16.27301 17.988281 17.988281 16.396083 17.988281 14.466797 A 0.98809878 0.98809878 0 0 0 17.986328 14.414062 C 17.884577 12.513831 16.190443 11.011719 14 11.011719 z M 14 12.988281 C 15.392231 12.988281 15.94197 13.610038 16.001953 14.492188 C 15.989803 15.348434 15.460091 16.011719 14 16.011719 C 12.614594 16.011719 11.988281 15.302225 11.988281 14.466797 C 11.988281 14.049083 12.140703 13.734298 12.460938 13.464844 C 12.78117 13.19539 13.295221 12.988281 14 12.988281 z M 11 19 A 1.0001 1.0001 0 0 0 10 20 L 10 39 A 1.0001 1.0001 0 0 0 11 40 L 17 40 A 1.0001 1.0001 0 0 0 18 39 L 18 33.134766 L 18 20 A 1.0001 1.0001 0 0 0 17 19 L 11 19 z M 20 19 A 1.0001 1.0001 0 0 0 19 20 L 19 39 A 1.0001 1.0001 0 0 0 20 40 L 26 40 A 1.0001 1.0001 0 0 0 27 39 L 27 29 C 27 28.170333 27.226394 27.345035 27.625 26.804688 C 28.023606 26.264339 28.526466 25.940057 29.482422 25.957031 C 30.468166 25.973981 30.989999 26.311669 31.384766 26.841797 C 31.779532 27.371924 32 28.166667 32 29 L 32 39 A 1.0001 1.0001 0 0 0 33 40 L 39 40 A 1.0001 1.0001 0 0 0 40 39 L 40 28.261719 C 40 25.300181 39.122788 22.95433 37.619141 21.367188 C 36.115493 19.780044 34.024172 19 31.8125 19 C 29.710483 19 28.110853 19.704889 27 20.423828 L 27 20 A 1.0001 1.0001 0 0 0 26 19 L 20 19 z M 12 21 L 16 21 L 16 33.134766 L 16 38 L 12 38 L 12 21 z M 21 21 L 25 21 L 25 22.560547 A 1.0001 1.0001 0 0 0 26.798828 23.162109 C 26.798828 23.162109 28.369194 21 31.8125 21 C 33.565828 21 35.069366 21.582581 36.167969 22.742188 C 37.266572 23.901794 38 25.688257 38 28.261719 L 38 38 L 34 38 L 34 29 C 34 27.833333 33.720468 26.627107 32.990234 25.646484 C 32.260001 24.665862 31.031834 23.983076 29.517578 23.957031 C 27.995534 23.930001 26.747519 24.626988 26.015625 25.619141 C 25.283731 26.611293 25 27.829667 25 29 L 25 38 L 21 38 L 21 21 z"></path>
@@ -63,51 +38,55 @@ const Homepage = () => {
                   </svg>
                 </span>
               </div>
-              <div className='hero-img '>
-                <img src={heroImage} className='morph'/>
-                <span className='overlay'/>
+              <div className='hero_img_box '>
+                <img className="hero_img_box " src={heroImage} className='morph'/>
               </div>
             </div>
             <div className='skills'>
-              <h3>Tech Stack</h3>
-              <ul>
-                  <li>
-                    <img src={image1}/>
-                  </li>
-                  <li><img src={image4} /></li>
-                  <li>
-                    <img src={image2}/>
-                  </li>
-                  <li>
-                    <img src={image3}/>
-                  </li>
-              </ul>
+              <div className='skills_box'>
+                <h3>Tech Stack</h3>
+                <ul className='skills_icons_list'>
+                    <li>
+                      <img src={image1}/>
+                    </li>
+                    <li><img src={image4} /></li>
+                    <li>
+                      <img src={image2}/>
+                    </li>
+                    <li>
+                      <img src={image3}/>
+                    </li>
+                </ul>
+              </div>
             </div>
-
           </div>
-        </div>
-        
       </section>
       <section id="about" className='about'>
         <div className='container'>
-          <div className='about-img'>
-            <img className="about-img-main" src={image6} />
-       
-            <img className='about-spinner ring' src={spiningImage}/> 
-            <img className='about-emoji' src={workingEmoji} />
+          <div className='about_img_box'>
+            <img className="about_img" src={image6} />
+            <img className='about_spinner ring' src={spiningImage}/> 
+            <img className='about_emoji' src={workingEmoji} />
           </div>
-          <div className='about-text'>
+          <div className='about_text'>
             <p className='title'>ABOUT ME</p>
-            <p className='sub-title'> A dedicated Front-end Developer 
+            <p className='sub-title'> Front-end Developer 
               <br/>based in Lagos, Nigeria 📍
             </p>
-            <p className='sub-text'>As a Mid-Sinior Front-End Developer, 
-              I possess an impressive arsenal of skills in HTML, CSS, JavaScript, React, Tailwind, and SCSS. 
-              I excel in designing and maintaining responsive websites that offer a smooth user experience.
+            <p className='sub-text'>
+              Hey, my name is Kingsley, and I'm a Frontend Developer.
+              My <br/> passion is to create and develop a clean UI/UX for my users. 
+            </p>
+            <p className='sub-text'>My main stack currently is React/Next.js in combination with <br/>  
+              Tailwind CSS and TypeScript.</p>
+              <p className='sub-text'>And I also work with Nodejs express and mongodb.</p>
+            {/* <p className='sub-text'>As a Mid-Sinior Front-End Developer, 
+              I possess an impressive arsenal of  <br/> skills in HTML, CSS,  JavaScript, React, Tailwind, and SCSS. <br/>
+              I excel in designing and maintaining responsive websites  <br/> that offer a smooth user experience.
                My expertise lies in crafting dynamic, engaging interfaces through writing clean and optimized code and utilizing 
                cutting-edge development tools and techniques. I am also a team player who thrives in collaborating with cross-functional teams 
                to produce outstanding web applications.
-            </p>
+            </p> */}
           </div>
         </div>
 
@@ -146,8 +125,8 @@ const Homepage = () => {
       </section>
       <section id='contact' className='contact'>
         <div className='contact_items'> 
-          <h4>CONTACT</h4>
-          <h3>Don't be shy! Hit me up! 👇</h3>
+          <span>CONTACT</span>
+          <h3>Don't be shy! Hit me up!👇</h3>
           <div class="contact__icons">
             <div class="contact__icon-box">
               <span>
@@ -163,10 +142,8 @@ const Homepage = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="tabler-icon tabler-icon-mail"><path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z"></path><path d="M3 7l9 6l9 -6"></path></svg>
               </span>
               <div class="contact__info">
-                <h3>Mail</h3>
-                <a href="mailto:ibenoahs@gmail.com">
-                ibenoahs@gmail.com
-                </a>
+                <h3>Email</h3><a href="mailto:ibenoahs@gmail.com">ibenoahs@gmail.com</a>
+              
               </div>
             </div>
           </div>
@@ -174,9 +151,6 @@ const Homepage = () => {
       </section>
 
       <Footer />
-
-      
-
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,14 +14,13 @@ const Navbar = () => {
     <div> 
       <nav className='nav'>
         <div className='nav-items'> 
-        <h2>
-          Kings.dev
-        </h2>
-        <svg  
-          onClick={handleToggle} 
-          xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mobile-menu"><path d="M4 6l16 0"></path><path d="M4 12l16 0"></path><path d="M4 18l16 0"></path>
-        </svg>
-        {/* <span><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="tabler-icon tabler-icon-x"><path d="M18 6l-12 12"></path><path d="M6 6l12 12"></path></svg></span> */}
+          <h2>
+            Kings.dev
+          </h2>
+          <svg  
+            onClick={handleToggle} 
+            xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mobile-menu"><path d="M4 6l16 0"></path><path d="M4 12l16 0"></path><path d="M4 18l16 0"></path>
+          </svg>
         <ul className='nav-list'>
           <li>
             <a href='#home'>Home </a>
@@ -39,12 +38,14 @@ const Navbar = () => {
         </div>
       </nav>
       {toggleMenu &&
-      <div className='mobile_nav'>
+            <div className={`${toggleMenu && "nav_animation"} mobile_nav `}>
+
+      {/* <div className='mobile_nav nav_animation'> */}
         <span>
           <svg onClick={handleToggle} xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="tabler-icon tabler-icon-x"><path d="M18 6l-12 12"></path><path d="M6 6l12 12"></path></svg>
         </span>
-        <div className='mobile_menu_items'>
-          <ul className='mobile_nav_nav_lis'>
+        {/* <div className='mobile_menu_items'> */}
+          <ul className='mobile_nav_list'>
               <li>
                 <a href='#home'>Home </a>
               </li>
@@ -58,7 +59,7 @@ const Navbar = () => {
                 <a href="#contact">Contact </a>
               </li>
           </ul>
-        </div>
+        {/* </div> */}
       </div>
       }
       
