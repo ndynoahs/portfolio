de2b79eed227b0072daff194ea851820bb234d29
kingsley <ndynoahs@gmail.com>
2024-04-20 11:14:57 +0100

    changed url to one of the projects

--- src/data.js
+++ src/data.js
@@ -23,7 +23,7 @@ export const products = [
       id: 2,
       imgSrc: rmfd,
       img: "https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-      link: "https://rightmotivefoundation.netlify.app/",
+      link: "https://www.rightmotivefoundation.org.ng/",
       alt: "rightmotivefoundation"
     },
     {
