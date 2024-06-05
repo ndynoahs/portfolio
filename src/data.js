fc02e703a6856d740be962242849307819d601a6
kingsley <ndynoahs@gmail.com>
2024-06-05 15:11:26 +0100

    added new project to portfolio

--- src/data.js
+++ src/data.js
@@ -1,93 +1,97 @@
-import setoop from "./assets/images/setoop.png"
-import xtreme from "./assets/images/xtreme.png"
-import tyny from "./assets/images/tyny.png"
-import chatty from "./assets/images/chatty.png"
-import wargame from "./assets/images/wargame.png"
+import setoop from "./assets/images/setoop.png";
+import xtreme from "./assets/images/xtreme.png";
+import tyny from "./assets/images/tyny.png";
+import chatty from "./assets/images/chatty.png";
+import wargame from "./assets/images/wargame.png";
 
-import sunnyside from "./assets/images/sunnyside.png"
-import acetube from "./assets/images/acetube.png"
-import rockPaper from "./assets/images/rockpaper.png"
-import gallary from "./assets/images/gallary-app.png"
-import htmlPortfolio from "./assets/images//html_portfolio.png"
-import rmfd from "./assets/images/rmdf.png"
+import sunnyside from "./assets/images/sunnyside.png";
+import acetube from "./assets/images/acetube.png";
+import rockPaper from "./assets/images/rockpaper.png";
+import gallary from "./assets/images/gallary-app.png";
+import klinfast from "./assets/images/klinfast.png";
+import htmlPortfolio from "./assets/images/html_portfolio.png";
+
+import rmfd from "./assets/images/rmdf.png";
 export const products = [
   // USE CHROME FULL PAGE EXTENTION
-    {
-      id: 1,
-      imgSrc:tyny,
-      img: "https://images.unsplash.com/photo-1594897030264-ab7d87efc473?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-      link:"https://tyny.dev",
-      alt: "tyny",
-    },
-    {
-      id: 2,
-      imgSrc: rmfd,
-      img: "https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-      link: "https://www.rightmotivefoundation.org.ng/",
-      alt: "rightmotivefoundation"
-    },
-    {
-      id: 3,
-      imgSrc: xtreme,
-      img: "https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-      link: "https://app.xtremecharacterchallenge.com",
-      alt: "xtreme"
-    },
-    {
-      id: 4,
-      imgSrc: setoop,
-      img: "https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-      link: "https://setoop.com",
-      alt: "setoop",
- 
-    },
-    {
-      id: 5,
-      imgSrc: chatty,
-      link: "https://chatty-f8fe.onrender.com/chats",
-      alt: "chatty"
-    },
-    {
-      id: 6,
-      imgSrc: htmlPortfolio,
-      link: "https://responsive-photo-gallery.netlify.app/",
-      alt: "hmtl portfolio",
-    },
-  
-    
-    {
-      id: 7,
-      imgSrc: rockPaper,
-      link: "https://rps-fungame.netlify.app/",
-      alt: "rock paper image"
-    },
-   
-    {
-      id: 8,
-      imgSrc: gallary,
-      link: "https://responsive-photo-gallery.netlify.app/",
-      alt: "gallary image"
-    },
-    {
-      id: 9,
-      imgSrc: wargame,
-      link: "https://amazingwargame.netlify.app/" , 
-      alt: "war game"
-    },
-    {
-      id: 10,
-      imgSrc: sunnyside,
-      link: "https://sunnysid.netlify.app",
-      alt: "sunnyside"
-    },
-    {
-      id: 11,
-      imgSrc: acetube,
-      link: "https://elegant-einstein-74aa53.netlify.app/",
-      alt: "ace tube"
-    },
- 
-    
-    
-  ];
-  
+
+  {
+    id: 1,
+    imgSrc: klinfast,
+    link: "https://amazingwargame.netlify.app/",
+    alt: "klinfast",
+  },
+  {
+    id: 2,
+    imgSrc: xtreme,
+    img: "https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    link: "https://app.xtremecharacterchallenge.com",
+    alt: "xtreme",
+  },
+  {
+    id: 3,
+    imgSrc: tyny,
+    img: "https://images.unsplash.com/photo-1594897030264-ab7d87efc473?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    link: "https://tyny.dev",
+    alt: "tyny",
+  },
+  {
+    id: 4,
+    imgSrc: rmfd,
+    img: "https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    link: "https://www.rightmotivefoundation.org.ng/",
+    alt: "rightmotivefoundation",
+  },
+
+  {
+    id: 5,
+    imgSrc: setoop,
+    img: "https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    link: "https://setoop.com",
+    alt: "setoop",
+  },
+  {
+    id: 6,
+    imgSrc: chatty,
+    link: "https://chatty-f8fe.onrender.com/chats",
+    alt: "chatty",
+  },
+  // {
+  //   id: 7,
+  //   imgSrc: htmlPortfolio,
+  //   link: "#",
+  //   alt: "hmtl portfolio",
+  // },
+
+  // {
+  //   id: 8,
+  //   imgSrc: gallary,
+  //   link: "https://responsive-photo-gallery.netlify.app/",
+  //   alt: "gallary image",
+  // },
+  // {
+  //   id: 9,
+  //   imgSrc: wargame,
+  //   link: "https://amazingwargame.netlify.app/",
+  //   alt: "war game",
+  // },
+
+  // {
+  //   id: 9,
+  //   imgSrc: rockPaper,
+  //   link: "https://rps-fungame.netlify.app/",
+  //   alt: "rock paper image",
+  // },
+  // {
+  //   id: 10,
+  //   imgSrc: sunnyside,
+  //   link: "https://sunnysid.netlify.app",
+  //   alt: "sunnyside",
+  // },
+  // {
+  //   id: 11,
+  //   imgSrc: acetube,
+  //   link: "https://elegant-einstein-74aa53.netlify.app/",
+  //   alt: "ace tube"
+  // },
+];
