db15d92b4089b7ffdfd83401ca8383d71d7a121f
kingsley <ndynoahs@gmail.com>
2024-07-04 10:53:56 +0100

    changed portfolio url

--- src/data.js
+++ src/data.js
@@ -25,7 +25,7 @@ export const products = [
     id: 2,
     imgSrc: xtreme,
     img: "https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-    link: "https://app.xtremecharacterchallenge.com",
+    link: "https://www.xtremecharacterchallenge.com/",
     alt: "xtreme",
   },
   {
